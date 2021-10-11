3d10ea7507f47feac036a8053bbfd8e4ea77cc9e
melsafi1 <56850296+melsafi1@users.noreply.github.com>
2021-10-11 02:33:18 -0700

    Update Program.cs

--- MultiThreadPi2/MultiThreadPi2/Program.cs
+++ MultiThreadPi2/MultiThreadPi2/Program.cs
@@ -10,10 +10,10 @@ namespace MultiThreadPi2
     {
         static void Main(string[] args)
         {
-            long numberOfSamples = 100000000;
+            long numberOfSamples = 1000000000;
             long hits_multi = 0;
             long hits_single = 0;
-            int num_threads = 10;
+            int num_threads = 12;
             long samples_per_thread = numberOfSamples / num_threads;
             Stopwatch stopwatch_singlethread = new Stopwatch();
             Stopwatch stopwatch_multithread = new Stopwatch();
